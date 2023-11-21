ba9b9c627b30a249a6534c8b6daf606bd357e6a4
Влад <@DESKTOP-QAU2T3T>
2023-11-21 15:41:31 +0400

    edit user from admin is working for now

--- KPO_Cursovaya/Models/User.cs
+++ KPO_Cursovaya/Models/User.cs
@@ -11,7 +11,7 @@ public partial class User
 
     public string? Email { get; set; }
 
-    public bool? EmailConfirmed { get; set; }
+    public bool EmailConfirmed { get; set; }
 
     public string Password { get; set; } = null!;
 
